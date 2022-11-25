a33e93562d53c866e39eabef850eed79de865f44
goncaloarsenio <goncalo.arsenio92@gmail.com>
2022-11-25 14:13:46 +0000

    partners changes

---
+++ src/pages/Partners.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import People from '../images/people.png'
+
+function Partners() {
+  return (
+    <div>
+
+    <div className="img">
+        <h1>Parceiros JobCoach</h1>
+        <p>Todos os nossos parceiros numa só página. Faz parte desta aventura e junta-te a nós!</p>
+        <img clasName="people" src={People} alt="" />
+    </div>
+
+    <div>
+    <h2>Os nossos parceiros</h2>
+    <p>Para quê trabalhar sozinho quando se pode trabalhar em conjunto e chegar ainda mais longe?
+    JobCoach cria parcerias e conecta pessoas para que todos saiam a ganhar.</p>
+    </div>
+
+    </div>
+  )
+}
+
+export default Partners
